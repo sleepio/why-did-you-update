a3e0fbb00f315b43edd24f91100b1cca262ee09b
Michael Schneider <mischneider1@gmail.com>
2018-01-23 09:21:53 -0800

    Add () to default excludeds to include HOC that often have brackets in them (#11)

--- src/normalizeOptions.js
+++ src/normalizeOptions.js
@@ -3,7 +3,7 @@ import _isString from 'lodash.isstring'
 import {defaultNotifier} from './defaultNotifier'
 
 export const DEFAULT_INCLUDE = /./
-export const DEFAULT_EXCLUDE = /[^a-zA-Z0-9]/
+export const DEFAULT_EXCLUDE = /[^a-zA-Z0-9()]/
 
 const toRegExp = s => _isString(s) ? new RegExp(`^${s}$`) : s
 const toArray = o =>  o ? [].concat(o) : []
