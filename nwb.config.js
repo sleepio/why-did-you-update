9b7496ee5104e44da19011d7f2c3f3e24f4f9d35
vzaidman <vzaidman@gmail.com>
2018-11-16 11:22:31 +0200

    using react hot loader

--- nwb.config.js
+++ nwb.config.js
@@ -1,23 +1,35 @@
-var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const _ = require('lodash');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 
-module.exports = {
-  type: 'react-component',
-  npm: {
-    umd: {
-      global: 'WhyDidYouUpdate',
-      externals: {
-        'react': 'React'
-      }
-    }
-  },
-  webpack: {
-    extra: {
-      plugins: [new LodashModuleReplacementPlugin()],
+module.exports = function({ command }) {
+  const needsRHL = command.startsWith('serve');
+
+  const mainConfig = {
+    type: 'react-component',
+    npm: {
+      umd: {
+        global: 'WhyDidYouUpdate',
+        externals: {
+          react: 'React',
+        },
+      },
     },
-  },
-  babel: {
-    plugins: [
-      'babel-plugin-lodash'
-    ]
-  }
+    webpack: {
+      config(webpackConfig) {
+        if (needsRHL) {
+          webpackConfig.entry.unshift('react-hot-loader/patch');
+        }
+        webpackConfig.plugins.unshift(new LodashModuleReplacementPlugin());
+        return webpackConfig;
+      },
+    },
+    babel: {
+      plugins: _.compact([
+        needsRHL && 'react-hot-loader/babel',
+        'babel-plugin-lodash',
+      ]),
+    },
+  };
+
+  return mainConfig;
 };
